3d73cc8ed58cc8c5fa16680b1f517cd138205735
Dylan Fox <dylanbfox@gmail.com>
2016-07-03 14:30:36 -0700

    SGD Adaline

--- ch2/perceptron.py
+++ ch2/perceptron.py
@@ -1,4 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+from matplotlib.colors import ListedColormap
 
 class Perceptron(object):
 	"""
@@ -49,10 +52,11 @@ class Perceptron(object):
 
 		"""
 
+		# initialize weights to zero-vector 
 		self.w_ = np.zeros(1 + X.shape[1])
 		self.errors_ = []
 
-		for _ in range(self..n_iter):
+		for _ in range(self.n_iter):
 			errors = 0
 			for xi, target in zip(X, y):
 				update = self.eta * (target - self.predict(xi))
@@ -71,5 +75,30 @@ class Perceptron(object):
 		step (ie, activation function)"""
 		return np.where(self.net_input(X) >= 0.0, 1, -1)
 
-		
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+
+	# setup marker generator and color map
+	markers = ('s', 'x', 'o', '^', 'v')
+	colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+	cmap = ListedColormap(colors[:len(np.unique(y))])
+
+	# plot decision surface
+	x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+	x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+	xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+						np.arange(x2_min, x2_max, resolution))
+
+	Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+	Z = Z.reshape(xx1.shape)
+	plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+	plt.xlim(xx1.min(), xx1.max())
+	plt.ylim(xx2.min(), xx2.max())
+
+	# plot class samples
+	for idx, cl in enumerate(np.unique(y)):
+		plt.scatter(x=X[y==cl, 0], y=X[y==cl, 1],
+					alpha=0.8, c=cmap(idx),
+					marker=markers[idx], label=cl)
+
+
 
